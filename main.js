699fc24a4fc8c64ebd9b74b72e91bf1721aea839
Sarah <sarahrdvieira@gmail.com>
2024-12-19 20:46:34 -0300

    Edit playGame function to output winner/loser messages + final scores

--- main.js
+++ main.js
@@ -19,50 +19,75 @@ const getHumanChoice = () => {
     return userInput;
 }
 
-    // Variables to keep track of the scores
-    let humanScore = 0;
-    let computerScore = 0;
-
 // Takes both user and computer's choices and finds the winner of the round
 const playRound = (humanChoice, computerChoice) => {
     // Make humanChoice case-insensitive
     humanChoice = humanChoice.toLowerCase();
+
+    // Variable that returns a value depending on the winner
+    let winner;
     
     // Define winner of round
     if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
+        winner = "human";
         console.log("You win! Paper beats Rock.");
     } else if (computerChoice === "paper" && humanChoice === "rock") {
-        computerScore++;
+        winner = "computer";
         console.log("You lose! Paper beats Rock.");
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore++;
+        winner = "human";
         console.log("You win! Rock beats Scissors.");
     } else if (computerChoice === "rock" && humanChoice === "scissors") {
-        computerScore++;
+        winner = "computer";
         console.log("You lose! Rock beats Scissors.");
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore++;
+        winner = "human";
         console.log("You win! Scissors beats Paper.");
     } else if (computerChoice === "scissors" && humanChoice === "paper") {
-        computerScore++;
+        winner = "computer";
         console.log("You lose! Scissors beats Paper.");
     } else {
         console.log("It's a tie!");
     }
+
+    return winner;
 }
 
 // Plays 5 rounds of the game
 const playGame = () => {
 
+    // Variables to keep track of the scores
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // Play round five times
     for (let i = 1; i < 6; i++) {
         const humanChoice = getHumanChoice();
         const computerChoice = getComputerChoice();
 
-        playRound(humanChoice, computerChoice);
+        const result = playRound(humanChoice, computerChoice);
+
+        // Increments score depending on the winner
+        if (result === "human") {
+            humanScore++;
+        } else if (result === "computer") {
+            computerScore++;
+        };
     }
 
-    console.log(humanScore, computerScore);
+    // Outputs message depending on the final score
+    if (humanScore > computerScore) {
+        console.log("You won! Congratulations!");
+    } else if (computerScore > humanScore) {
+        console.log("You lost! Try again!");
+    } else {
+        console.log("It's a tie! Oops!");
+    }
+
+    // Outputs final score of both players
+    console.log("Your score: " + humanScore + "\n" +
+        "Computer's score: " + computerScore
+    );
 }
 
 playGame();
