0fda18900fe75c32aafcec4339a04e35c7172cec
Sarah <sarahrdvieira@gmail.com>
2024-12-19 19:47:11 -0300

    Edit getComputerChoice function

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// Randomly returns a string value between "rock", "paper" or "scissors"
+const getComputerChoice = () => {
+    // Randomly pick a random number between 1 and 3
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+    // Return a specific string depending on which number is picked
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
